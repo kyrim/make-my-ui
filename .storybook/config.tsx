e68b4285fec4daece0dc0c04e9ce3502cf514ac3
kyrimsteele <kyrimcsteele@gmail.com>
2019-12-17 23:41:19 +1100

    Add new theme to docs

--- .storybook/config.tsx
+++ .storybook/config.tsx
@@ -1,8 +1,15 @@
 import React from "react";
-import { configure, addDecorator } from "@storybook/react";
+import { configure, addDecorator, addParameters } from "@storybook/react";
 import { ThemeProvider } from "styled-components";
 
+import theme from "./theme";
 import { GlobalStyle, defaultTheme } from "../src/styles";
+
+addParameters({
+  options: {
+    theme: theme
+  }
+});
 
 //automatically import all files ending in *.stories.js
 const uiReq = require.context("../src", true, /.stories.tsx$/);
---
+++ .storybook/theme.js
@@ -0,0 +1,19 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "light",
+  // UI
+  appBg: "white",
+  appContentBg: "white",
+  appBorderColor: "black",
+  appBorderRadius: 10,
+  inputBorder: "black",
+
+  fontBase: '"Open Sans", sans-serif',
+
+  barTextColor: "gray",
+  barBg: "white",
+
+  brandTitle: "MakeMyUI",
+  brandUrl: "https://github.com/kyrim/make-my-ui"
+});

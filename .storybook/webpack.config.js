f01d42a2335a9c01182f1312445c4edf3c02d86c
kyrimsteele <kyrimcsteele@gmail.com>
2020-09-12 11:32:58 +1000

    Fix breaking build

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -6,17 +6,12 @@ module.exports = ({ config, mode }) => {
     test: /\.(ts|tsx)$/,
     loader: require.resolve("babel-loader"),
     options: {
-      presets: [require.resolve("babel-preset-react-app")]
-    }
+      presets: [require.resolve("babel-preset-react-app")],
+    },
   });
 
   config.resolve.extensions.push(".ts", ".tsx");
 
-  config.plugins.push(
-    new ForkTsCheckerWebpackPlugin({
-      async: false,
-      checkSyntacticErrors: true
-    })
-  );
+  config.plugins.push(new ForkTsCheckerWebpackPlugin());
   return config;
 };

d4d2959a3670e204fd4be43fd0771820040256be
kyrimsteele <kyrimcsteele@gmail.com>
2020-09-13 00:22:30 +1000

    Add new packages and remove debug line

--- src/autocomplete/index.tsx
+++ src/autocomplete/index.tsx
@@ -154,7 +154,6 @@ const Autocomplete = ({
   inputProps,
   ...restOfProps
 }: Props) => {
-  debugger;
   const onChangeMemo = useCallback(
     (selection: AutocompleteItem | null) => selection && onSelect(selection),
     []
